9e09c7d081b96ec64539e0dc18cad650c4e0da4b
charlesfloyd <charlesfloyd.github@gmail.com>
2020-09-25 23:26:56 -0400

    figured out varying bar height without moving positions

--- ccrb/ccrb.js
+++ ccrb/ccrb.js
@@ -53,7 +53,6 @@ window.onload = () => {
 	{name:'Resolution Months', col:'resolution_months'},
 	{name:'Complaint Result', col:'complaint_result'},
     ].forEach((d, i) => {
-//	console.log('i is', i);
 	d['g'] = g_of_xy_mult(g_xmult, g_ymult);
 	d['max_h'] = 0.1 * h * (1 - g_ymult) * (i+1);
 	d['min_h'] = 0.1 * h * (1 - g_ymult) * i;
@@ -62,32 +61,51 @@ window.onload = () => {
 	gs[d.name] = d
     });
 };
+
 global.filters = [];
+
+const initialize_gs = (data) => {
+    Object.entries(gs).forEach((d) => {
+	let curr = d[1];
+	curr['xs'] = {};
+	let counts = Object.entries(ccrblib.organize_data(data, curr.col, []));
+	let n = counts.length;
+	let rect_w = curr.max_w / n;
+	let max_label_w = d3.max(counts, (ar) => ar[0].length + 1);
+	let font_size = 1.75 * rect_w / max_label_w;
+	curr['font_size'] = font_size;
+	curr['width'] = rect_w;
+	counts.forEach((c, i) => {
+	    // console.log(c[0], i);
+	    curr.xs[c[0]] = rect_w * i;
+	});
+	curr.g.selectAll('text')
+	    .data(counts)
+	    .enter()
+	    .append('text')
+	    .attr('x',(d) => curr.xs[d[0]]) //,i)=>rect_w * (i+0.5))
+	    .attr('y',(d) => {return curr.max_h})
+	    .style('font-size', font_size)
+	    .text((d)=>d[0])
+	    .attr('fill','blue');
+    });
+    update_gs(data, []);
+};
+
 const update_gs = (data, filters) => {
-    // console.log(margin, w, h);
-    // console.log('here');
-    // console.log('data has length', data.length, typeof data);
     Object.entries(gs).forEach((d) => {
 	let curr = d[1];
 	let label = d[0];
-//	console.log(curr);
+	let font_size = curr.font_size;
 	let g = curr.g;
 	let counts = Object.entries(
 	    ccrblib.organize_data(data, curr.col, filters)
 	);
 	let max_count = d3.max(counts, (ar) => ar[1]) * 1.1;
-//	console.log(counts, 'max_count is',max_count);
-	let n = counts.length;
-	let rect_w = curr.max_w / n;
-	let max_label_w = d3.max(counts, (ar) => ar[0].length + 1);
-	let font_size = 1.75 * rect_w / max_label_w;
-	font_size = Math.min(font_size, (curr.max_h - curr.min_h) / 6);
-//	console.log('font_size is', font_size);
 	var scale = d3.scaleLinear()
 	    .domain([0, max_count])
 	    .range([curr.max_h - font_size, curr.min_h]);
 	let rect_h = (ar) => {return curr.max_h - font_size - scale(ar[1])};
-//	counts.forEach((d) => console.log(d[1],d[0],scale(d[1])));
 	let axis = d3.axisLeft()
 	    .scale(scale)
 	    .ticks(5);
@@ -95,11 +113,9 @@ const update_gs = (data, filters) => {
 	    .data(counts)
 	    .enter()
 	    .append('rect')
-	    .attr('x',(d,i) => {return rect_w * i})
-//	    .attr('y',(d) => curr.max_h - rect_h(d) + curr.j * curr.max_h)
+	    .attr('x',(d,i) => curr.xs[d[0]]) //return rect_w * i})
 	    .attr('y',(d,i) => {return scale(d[1])})
-	    .attr('width', rect_w)
-	//	    .attr('height', rect_h);
+	    .attr('width', curr.width)
 	    .attr('height', rect_h)
 	    .on('click', (d) => {
 		// console.log(d, filters, typeof filtersz);
@@ -114,28 +130,13 @@ const update_gs = (data, filters) => {
 		console.log('filters is:', filters);
 		Object.entries(gs).forEach((x) => {
 		    x[1].g.selectAll('rect').remove();
-		    x[1].g.selectAll('text').remove();
 		});
 		update_gs(data, filters);
-	    }
-	       );
-	g.selectAll('text')
-	    .data(counts)
-	    .enter()
-	    .append('text')
-	    .attr('x',(d,i)=>rect_w * (i+0.5))
-//	    .attr('y',curr.max_h * (curr.j + 1) + font_size + 1)
-	    .attr('y',(d) => {return curr.max_h})
-	    .style('font-size', font_size)
-	    .text((d)=>d[0])
-	    .attr('fill','blue');
-	// g.append('g')
-	//     .call(axis);
-	
+	    });
     });
-};
+}
 
-const initialize_gs = (data) => update_gs(data, [])
+//const initialize_gs = (data) => update_gs(data, [])
 
 d3.csv('./allegations.csv').then(
     (data) => {
